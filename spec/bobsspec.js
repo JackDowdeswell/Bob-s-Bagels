800472d61dd52e9b2f6b75c2fcc253f16f8207d4
Jack Dowdeswell <jack.dowdeswell@outlook.com>
2021-10-13 17:09:28 +0100

    test 2 attempt

--- spec/bobsspec.js
+++ spec/bobsspec.js
@@ -4,11 +4,29 @@ const assertEquals = require('../test-framework.js');
 
 let basket1;
 
+/*
+console.log('Test 1: add 1 item to basket');
 //Setup (Arrange)
 basket1 = new Basket(0);
 
 let input = ['Sesame Bagel'];
 let expectedOutput = 1;
+
+//Execute (Act)
+let actualOutput = basket1.addToBasket(input);
+
+//Verify (Assert)
+let result = assertEquals(actualOutput, expectedOutput);
+console.log(`Number of items added to the basket: ${input.length}`);
+console.log(result);
+*/
+
+console.log('Test 2: add 2 items to basket');
+//Setup (Arrange)
+basket1 = new Basket(2);
+
+let input = ['Sesame Bagel', 'Cheese Bagel'];
+let expectedOutput = 4;
 
 //Execute (Act)
 let actualOutput = basket1.addToBasket(input);
--- src/BobsBagels.js
+++ src/BobsBagels.js
@@ -2,17 +2,16 @@ class Basket {
 
     // properties
     numberOfItems = 0;
-    basket;
 
-    constructor(numberOfItems, Basket) {
+    constructor(numberOfItems) {
         // set values for object instance
         this.numberOfItems = numberOfItems;
-        this.basket = Basket;
     }
 
     // messages
-    addToBasket(input) {
-        input = 1;
+    addToBasket(num) {
+        num = 1;
+        return num;
     }
 }
 
